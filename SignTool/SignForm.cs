79892e6b13415ddf3066d391d42e34f704a4e41d
冰糖XH <76904365+bingtangxh@users.noreply.github.com>
2026-02-16 18:47:04 +0800

    Add overrideFBD

--- SignTool/SignForm.cs
+++ SignTool/SignForm.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace SignTool1
 {
+    
     public partial class SignForm : Form
     {
         public SignForm()
@@ -39,10 +41,10 @@ namespace SignTool1
             bool isAnyErrorHappened=false;
 
             FolderBrowserDialog folderDialog;
-
+            
             folderDialog = new FolderBrowserDialog();
 
-            if (folderDialog.ShowDialog() == DialogResult.OK)
+            if (folderDialog.ShowDialog(this) == DialogResult.OK)
             {
                 isAnyErrorHappened=Program.SignFolder(folderDialog.SelectedPath, message => logtxt.Text+=message, message => logtxt.Text+=message+Environment.NewLine);
                 
@@ -52,6 +54,29 @@ namespace SignTool1
                     MessageBox.Show("All Done!");
                 }
             }
+            /*
+             string folderPath="";
+            //FolderBrowserDialog folderDialog;
+
+            //folderDialog = new FolderBrowserDialog();
+            OpenFileDialog folderDialog = new OpenFileDialog();
+            folderDialog.ValidateNames=false;
+            folderDialog.CheckFileExists=false;
+            folderDialog.CheckPathExists=true;
+
+            if(folderDialog.ShowDialog()==DialogResult.OK) {
+                folderPath=Path.GetDirectoryName(folderDialog.FileName);
+                logtxt.Text+=folderPath+Environment.NewLine;
+
+                isAnyErrorHappened=Program.SignFolder(folderPath,message => logtxt.Text+=message,message => logtxt.Text+=message+Environment.NewLine);
+
+                if(isAnyErrorHappened) {
+                    MessageBox.Show("Done with some errors! Please check the log for details.");
+                } else {
+                    MessageBox.Show("All Done!");
+                }
+            } else return;
+             */
         }
     }
 }
